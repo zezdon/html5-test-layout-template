7d5c24d241a15a31a1b537ea2497643eb503665c
Mats G <ed54res@hotmail.com>
2021-04-08 22:08:43 +0200

    4. Team Part1 ThreeJs_Basic

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ $(window).on('load', function () { //make sure that whole site is loaded
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+/* ==================================
+           Team
+======================================*/
+$(function(){
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 300,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>' , '<i class="fa fa-angle-right"></i>']
+    });
+  });
